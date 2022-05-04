34244ea361cee46bbb93bae89de0f498a3610ba4
zidol <ajh3166@naver.com>
2022-05-04 16:59:43 +0900

    Initial commit

---
+++ src/main/java/kr/co/aihome/AihomeApplication.java
@@ -0,0 +1,13 @@
+package kr.co.aihome;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AihomeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AihomeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kr/co/aihome/AihomeApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.aihome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AihomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
