376f7eff2d19a6672a234ebe8408fc53e71f8fcb
zidol <ajh3166@naver.com>
2022-08-05 23:47:32 +0900

    테스트코드 세팅

--- src/test/java/kr/co/aihome/security/RefreshTokenServiceTest.java
+++ src/test/java/kr/co/aihome/security/RefreshTokenServiceTest.java
@@ -1,7 +1,21 @@
 package kr.co.aihome.security;
 
+import kr.co.aihome.repository.refreshtoken.RefreshTokenRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class RefreshTokenServiceTest {
     //TODO 테스트 코드 짜기!!
+
+    @Autowired
+    RefreshTokenRepository refreshTokenService;
+
+    @Test
+    void getRefreshToken() {
+
+    }
 }
